9ec29c5998112ae4fe4d7fe08ec30400b2b1cf93
maxwell Mullin <mullinmax@gmail.com>
2018-06-24 17:04:29 -0400

    can read files

---
+++ js/index.js
@@ -0,0 +1,20 @@
+function runTrinity() {
+  //   console.log(document.getElementById("rootFile").files[0]);
+  readText(document.getElementById("rootFile")).then(function(data) {
+    console.log(data);
+  });
+}
+var reader = new FileReader();
+/**
+ * read text input
+ */
+function readText(filePath) {
+  var text = new Promise(function(resolve, reject) {
+    var reader = new FileReader();
+    reader.onload = function(e) {
+      return resolve(e.target.result);
+    }; //end onload()
+    reader.readAsText(filePath.files[0]);
+  });
+  return text;
+}
