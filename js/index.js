0560609eaf5c504bbdf27869d414b3ed656ab295
maxwell Mullin <mullinmax@gmail.com>
2018-07-09 22:08:22 -0400

    added ability to save

--- js/index.js
+++ js/index.js
@@ -9,15 +9,17 @@ function runTrinity() {
   }
   Promise.all(texts).then(function(values) {
     console.log(values);
+    document.getElementById("saveButton").addEventListener("click", function() {
+      download("index.html", values[0]);
+    });
   });
   //   var compiledProject = trinity(data);
   // var newWindow = window.open();
   // newWindow.document.write(compiledProject);
 }
 var reader = new FileReader();
-/**
- * read text input
- */
+
+// read text input
 function readText(file) {
   var text = new Promise(function(resolve, reject) {
     var reader = new FileReader();
@@ -27,4 +29,21 @@ function readText(file) {
     reader.readAsText(file);
   });
   return text;
+}
+
+// Download text as file
+function download(filename, text) {
+  var element = document.createElement("a");
+  element.setAttribute(
+    "href",
+    "data:text/plain;charset=utf-8," + encodeURIComponent(text)
+  );
+  element.setAttribute("download", filename);
+
+  element.style.display = "none";
+  document.body.appendChild(element);
+
+  element.click();
+
+  document.body.removeChild(element);
 }
